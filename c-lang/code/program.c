c66538dfbc0767c15c91812a9e2838583cea96b4
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-12-16 10:56:57 +0530

    update

--- c-lang/code/program.c
+++ c-lang/code/program.c
@@ -1,1 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+
+    fp = fopen("sample.txt", "r");
+
+    if (fp == NULL) {
+        printf("File not found or cannot be opened.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        printf("%c\n", ch);   
+    }
+
+    fclose(fp);
+    return 0;
+}
